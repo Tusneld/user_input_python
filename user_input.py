b7798cddf12cca1468e57d240f06e17a841845f9
Tusnelde Endjala <tusneldelainae@gmail.com>
2024-03-13 18:40:52 +0200

    Getting input from the user

---
+++ user_input.py
@@ -0,0 +1,11 @@
+name = "Tusnelde"
+age = 26
+location = "Namibia"
+
+# Ask the user for their name
+name = input("What is your name? ")
+age = input("What is your age? ")
+location = input("Where are you from? ")
+
+print(f"Hello {name}, you are {age} years old and live in {location}.")
+
